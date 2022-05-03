7346c381c6b43c05df3f671b1858a09e88b65689
Alex <alrodriguez41@poligran.edu.co>
2022-05-03 15:09:26 -0500

    programación reactiva y bucles , minuto 13

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,32 @@
 <script>
-	let contador = 0;
-	let textColor = 'text-primary'
-	
-	const aumentar = () => {
-		contador ++
-		/*
-		if(contador > 5){
-			textColor = 'text-danger'
-		}*/
+	const cursos = ['html','css','js']
+	let numeros = [2,5,7,10,12]
+
+	const agregarNumero = () => {
+		let num = Math.floor(Math.random()*(20 -1))+1;
+		console.log(num)
+		/*numeros.push(num)
+		Los 3 puntos antes del array indica que vamos a copiar los valores del array
+		*/
+		numeros = [...numeros,num]
 	}
-	const reiniciar = () => {
-		contador = 0
-		
-		//textColor = 'text-primary'
-	}
-
-	//Declaracion reactiva
-	$: if(contador > 5){
-		textColor = 'text-danger'
-	}else{
-		textColor = 'text-primary'
-	}
-
-	$: doble =  contador * 2
-	$: mensaje = contador > 5 ? 'Es mayor a 5' : 'Es menor a 5'
-
-
 </script>
 
-<main>
-	<h1 class={textColor}>Counter: {contador}</h1>
+<main class="container">
+	<h1>Cursos</h1>
+	<ul>
+		{#each cursos as curse, index}
+			 <li>{index +1} - {curse}</li>
+		{/each}
+	</ul>
 
-	<h2>{mensaje}</h2>
+	<button on:click={agregarNumero} class="btn btn-primary">Agregar número</button>
 
-	<h3>Doble {doble}</h3>
-	<button on:click={aumentar}>Aumentar</button>
+	<ul>
+		<p>Números</p>
+		{#each numeros as num}
+			 <li>{num}</li>
+		{/each}
+	</ul>
 
-	<button on:click={reiniciar}>reiniciar</button>
 </main>
-
-<style>
-	.text-primary{
-		color:blueviolet;
-	}
-
-	.text-danger{
-		color: orangered;
-	}
-
-</style>
