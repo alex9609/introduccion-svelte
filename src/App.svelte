57288c69a092f8bbaf61de285fa509df0708208c
Alex <alrodriguez41@poligran.edu.co>
2022-05-04 11:42:28 -0500

    Vimos los bindings o inputs y como interactuan mediante programación reactiva en el frontedn

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,53 @@
 <script>
-	const cursos = ['html','css','js']
-	let numeros = [2,5,7,10,12]
+	let texto = ''
+	let numero = 0
+	let rango = 0
+	//Cuando tenemos un checkbox siempre tenemos dos valores
+	//true o false
+	let check = false
 
-	const agregarNumero = () => {
-		let num = Math.floor(Math.random()*(20 -1))+1;
-		console.log(num)
-		/*numeros.push(num)
-		Los 3 puntos antes del array indica que vamos a copiar los valores del array
-		*/
-		numeros = [...numeros,num]
-	}
+	let frutas = ['tomate','palta','frutilla']
+	let radio =''
+	let arrayResultado = []
+
+	$: console.log(arrayResultado)
 </script>
 
-<main class="container">
-	<h1>Cursos</h1>
-	<ul>
-		{#each cursos as curse, index}
-			 <li>{index +1} - {curse}</li>
-		{/each}
-	</ul>
+<div>
+	<h1>Bindings</h1>
+	<h2>Texto</h2>
+	<input type="text" bind:value={texto}>
+	<p>texto : {texto}</p>
 
-	
-	{#if numeros.length > 5}
-		 <!-- Condicional if -->
-		 <h3>Agregaste más de 5 numeros </h3>
-		 {:else}
-		 <button on:click={agregarNumero} class="btn btn-primary">Agregar número</button>		 
-	{/if}
-	<ul>
-		<p>Números</p>
-		{#each numeros as num}
-			 <li>{num}</li>
-		{/each}
-	</ul>
+	<h2>Number</h2>
+	<input type="number" bind:value={numero}>
+	<p>numero : {numero}</p>
 
-</main>
+	<h2>Rango</h2>
+	<input type="range" bind:value={rango} min=0 max=10>
+	<p>rango : {rango}</p>
+
+	<h3>checkbox</h3>
+	<input type="checkbox" bind:checked={check}>
+	Aceptar politicas de la empresa
+	<button disabled={!check}>Enviar</button>
+	<p>checkbox : {check}</p>
+
+	<h3>Array de frutas</h3>
+	{#each frutas as fruta}
+		<br>
+		<input type="radio" value={fruta} bind:group={radio}>
+		{fruta}
+	{/each}
+	<p>fruta: {radio}</p>
+
+	<h3>Multiples opciones</h3>
+	{#each frutas as fruta}
+		<br>
+		<input type="checkbox" value={fruta} bind:group={arrayResultado}>
+		{fruta}
+	{/each}
+	<p>array resultado: {arrayResultado}</p>
+
+</div>
+
